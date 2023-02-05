623157544c3815b5179593a916e756ec15947faa
Patrick Feiring <pfeiring@live.com>
2023-02-05 15:12:11 +0100

    Update highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,8 +1,12 @@
 [
+  (NULL)
+  "DAY"
+] @constant.builtin
+
+[
   (true)
   (false)
-  (NULL)
-] @constant.builtin
+] @boolean
 
 [
   (integer)
@@ -10,26 +14,72 @@
   (bit)
 ] @number
 
+(float) @float
 (string) @string
+
+(comment) @comment
 
 (call 
   function: (identifier) @function)
 
+(data_type) @type
+
 [
-  "NOT"
-  "AND"
+  "%"
   "&&"
+  "/"
+  "AND"
+  "BETWEEN"
+  "DIV"
+  "IN"
+  "INTERVAL"
+  "LIKE"
+  "MOD"
+  "NOT"
   "OR"
+  "REGEXP"
   "||"
 ] @operator
 
 [
+  ";"
+  "("
+  ")"
+] @punctuation
+
+[
   "AS"
   "ASC"
+  "CREATE TABLE"
+  "DEFAULT"
+  "DELETE FROM"
   "DESC"
+  "DESCRIBE"
+  "DROP"
+  "EXPLAIN"
   "FROM"
+  "GROUP BY"
+  "IF EXISTS"
+  "IF NOT EXISTS"
+  "INNER"
+  "IGNORE"
+  "INSERT"
+  "INTO"
+  "IS"
+  "JOIN"
+  "LEFT"
   "LIMIT"
   "OFFSET"
+  "ON DUPLICATE KEY UPDATE"
+  "ORDER BY"
+  "RIGHT"
   "SELECT"
+  "SHOW"
+  "TABLE"
+  "TABLES"
+  "TEMPORARY"
+  "TRUNCATE"
+  "USING"
+  "VALUES"
   "WHERE"
 ] @keyword
