a1aa9855a0c2fc507e54f8acb13773b866909144
Patrick Feiring <pfeiring@live.com>
2023-02-09 17:56:23 +0100

    feat: add create database highlights

---
+++ test/highlight/keywords.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS my_database CHARACTER SET utf8mb4 COLLATE latin1_german2_ci ENCRYPTION='Y';
+-- <- keyword
+--              ^ keyword
+--                                        ^ keyword
+--                                                              ^ keyword
+--                                                                                        ^ keyword
